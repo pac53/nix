a34f5adaf2900a306b491cfa431692492a372c66
pac53 <dylan.krecker@protonmail.com>
2022-04-02 23:49:36 -0500

    Little changes

--- home.nix
+++ home.nix
@@ -1,21 +1,35 @@
 {config, pkgs, ...}:	{
+  nixpkgs.config.allowUnfree = true;
+
 	home = {
 		username = "dylankr53";
 		homeDirectory = "/home/dylankr53";
+		packages = with pkgs; [
+  		unityhub
+  		jetbrains.rider
+  		jetbrains.clion
+		];
 	};
 
 	programs = {
+
 		kakoune = {
 			enable = true;
+			plugins = with pkgs.kakounePlugins; [
+  			powerline-kak
+			];
 			config = {
 				tabStop = 2;
 				indentWidth = 2;
 				colorScheme = "solarized-dark";
 				numberLines = {
   				enable = true;
-  				relative = true;
 				};
 			};
+			extraConfig = ''
+				powerline-start
+				powerline-theme solarized-dark
+			'';
 		};
 
 		git = {
@@ -25,5 +39,5 @@
 		};
 
 		home-manager.enable = true;
-	};
+	};	
 }
