ecc1a8a1c63d2d6af94992ead40a3e0ec08e6949
pac53 <102572149+pac53@users.noreply.github.com>
2022-04-09 19:48:07 -0500

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,48 +1,8 @@
-{config, pkgs, ...}:	{
-  nixpkgs.config.allowUnfree = true;
-
-	home = {
-		username = "dylankr53";
-		homeDirectory = "/home/dylankr53";
-		packages = with pkgs; [
-  		unityhub
-		] ++ (with jetbrains; [
-  		clion
-  		rider
-  		pycharm-professional
-		]);
-	};
-
-	programs = {
-
-		kakoune = {
-			enable = true;
-			plugins = with pkgs.kakounePlugins; [
-  			powerline-kak
-			];
-			config = {
-				tabStop = 2;
-				indentWidth = 2;
-				colorScheme = "solarized-dark";
-				numberLines = {
-  				enable = true;
-				};
-			};
-			extraConfig = ''
-				powerline-start
-				powerline-theme solarized-dark
-			'';
-		};
-
-		git = {
-  		enable = true;
-  		userName = "pac53";
-  		userEmail = "dylan.krecker@protonmail.com";
-		};
-
-		emacs = {
-  		enable = true;
-		};
-		home-manager.enable = true;
-	};	
+{config, pkgs, ...}:    {
+        programs = {
+                home-manager.enable = true;
+                neovim = {
+                        enable = true;
+                };
+        };
 }
