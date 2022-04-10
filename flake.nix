a13e7e4db22d4081b3c613e17267f547f2c62903
pac53 <102572149+pac53@users.noreply.github.com>
2022-04-09 19:48:45 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,18 @@
 {
-	description = "My Nix config.";
-	inputs = {
-		home-manager.url = "github:nix-community/home-manager/release-21.11";
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-	};
+        inputs = {
+                home-manager = {
+                        url = "github:nix-community/home-manager";
+                        inputs.nixpkgs.follows = "nixpkgs";
+                };
+                nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        };
 
-	outputs = {home-manager, ...}:
-		let
-			system = "x86_64-linux";
-			username = "dylankr53";
-		in {
-			homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-				configuration = import ./home.nix;
-				inherit system username;
-				homeDirectory = "/home/${username}";
-				stateVersion = "21.11";
-			};
-		};
+        outputs = {home-manager, ...}: {
+                homeConfigurations."dylankr" = home-manager.lib.homeManagerConfiguration {
+                        configuration = import ./home.nix;
+                        username = "dylan";
+                        homeDirectory = "/home/dylankr";
+                        system = "x86_64-linux";
+                };
+        };
 }
