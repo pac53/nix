ec99969186f73c783d43f13d5a82d1c72eec103b
pac53 <102572149+pac53@users.noreply.github.com>
2022-04-13 21:42:45 -0500

    few changes

--- home.nix
+++ home.nix
@@ -1,46 +1,49 @@
-{config, pkgs, ...}:	{
-	nixpkgs.config.allowUnfree = true;
+{config, pkgs, ...}:    {
+        nixpkgs.config.allowUnfree = true;
 
-# Extra packages
-	home = {
-		packages = with pkgs; [
-      mpv
-			qemu
-      discord
-			minecraft
-		];
-	};
+        # Extra packages
+        home = {
+                packages = with pkgs; [
+                        mpv
+                        texlive.combined.scheme-basic
+                        discord
+                        minecraft
+                ];
+        };
 
-	programs = {
-	  git = {
-		  enable = true;
-			userEmail = "dylan.krecker@protonmail.com";
-    };
-		home-manager.enable = true;
-		neovim = {
-			enable = true;
-			extraConfig = builtins.readFile ./init.vim;
-			plugins = with pkgs.vimPlugins; [
-				# Themes
-				vim-airline
-				vim-airline-themes
-				vim-colors-solarized
-				# Plugins for LSP	
-				nvim-lspconfig
-				completion-nvim
-			];
-			extraPackages = with pkgs; [
-				rnix-lsp
-				haskell-language-server
-				ccls
-      ];
-			# Other miscillaneous settings
-			viAlias = true;
-			withPython3 = false;
-			withRuby = false;
-		};
-  };
+        programs = {
+        git = {
+                enable = true;
+                userEmail = "dylan.krecker@protonmail.com";
+        };
+                home-manager.enable = true;
+                neovim = {
+                        enable = true;
+                        extraConfig = builtins.readFile ./init.vim;
+                        plugins = with pkgs.vimPlugins; [
+                                # Themes
+                                vim-airline
+                                vim-airline-themes
+                                vim-colors-solarized
+                                # Plugins for LSP
+                                nvim-lspconfig
+                                completion-nvim
+                                nerdcommenter
+                        ];
+                        extraPackages = with pkgs; [
+                                rnix-lsp
+                                haskell-language-server
+                                ccls
+                                texlab
+                        ];
+                        # Other miscillaneous settings
+                        viAlias = true;
+                        withPython3 = false;
+                        withRuby = false;
+                };
+
+                zsh = {
+                        enable = true;
+                };
+        };
 }
-
-
-
