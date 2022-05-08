4cf73672f56dc8138cf65ab2d08360e8d0347bcb
dylan <dylan@penguin>
2022-05-08 02:49:09 -0500

    bs

--- flake.nix
+++ flake.nix
@@ -7,15 +7,16 @@
 		};
 	};
 
-	let
-		username = "dylan";
-	in outputs = { home-manager, ... }: {
-		homeConfigurations.${userame} = home-manager.lib.homeManagerConfiguration {
-			inherit username
-			configuration = ./home.nix;
-			system = "x86_64-linux";
-			homeDirectory = "/home/${username};
-			stateVersion = "21.11";
+	outputs = { home-manager, ... }: 
+		let
+			username = "dylan";
+		in {
+			homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+				inherit username;
+				configuration = ./home.nix;
+				system = "x86_64-linux";
+				homeDirectory = "/home/${username}";
+				stateVersion = "21.11";
+			};
 		};
-	};
 }
